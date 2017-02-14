40d8c38c12d35bd0092ec6d3d449d8f6bfdec114
easydaniel <whitetiger50512@gmail.com>
2017-02-15 00:16:06 +0800

    simple navigation with tabs and nested route

--- app/components/Application.js
+++ app/components/Application.js
@@ -2,35 +2,23 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { Scene, Router } from 'react-native-router-flux';
 import { Text } from 'react-native';
-import EntypoIcon from 'react-native-vector-icons/Entypo';
-import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
 // Store
 import configureStore from '../store';
 
 // Components
-import Counter from './Counter';
-import Todo from './Todo';
 import RedScene from './RedScene';
 import BlueScene from './BlueScene';
 import YellowScene from './YellowScene';
 import TealScene from './TealScene';
 import ModalScene from './ModalScene';
+import Base from './Base';
 
 import styles from '../styles/application';
 
 // TabIcon
 const TabIcon = ({ selected, title }) => (
   <Text style={{ color: selected ? 'red' : 'black' }}>{title}</Text>
-);
-
-// Counter Tab Icon
-const CounterTabIcon = ({ selected }) => (
-  <EntypoIcon name="calculator" size={26} style={{ color: selected ? 'red' : 'black' }} />
-);
-// Todo Tab Icon
-const TodoTabIcon = ({ selected }) => (
-  <MaterialIcon name="assignment" size={26} style={{ color: selected ? 'red' : 'black' }} />
 );
 
 export default class Application extends Component {
@@ -57,23 +45,7 @@ export default class Application extends Component {
               tabBarStyle={styles.tabBar}
             >
               {/* Tab and it's scenes */}
-              <Scene key="counterTab" title="Counter" icon={CounterTabIcon}>
-                <Scene
-                  key="counter"
-                  component={Counter}
-                  title="Counter App"
-                />
-              </Scene>
-              <Scene key="todoTab" title="Counter" icon={TodoTabIcon}>
-                <Scene
-                  hideNavBar
-                  key="todo"
-                  component={Todo}
-                  title="Todo App"
-                />
-              </Scene>
-              {/* Tab and it's scenes */}
-              <Scene key="two" title="Tab two" icon={TabIcon}>
+              <Scene key="one" title="Tab one" icon={TabIcon}>
                 <Scene
                   key="red"
                   component={RedScene}
@@ -87,7 +59,7 @@ export default class Application extends Component {
               </Scene>
 
               {/* Tab and it's scenes */}
-              <Scene key="three" title="Tab three" icon={TabIcon}>
+              <Scene key="two" title="Tab two" icon={TabIcon}>
                 <Scene
                   key="yellow"
                   component={YellowScene}
@@ -97,6 +69,15 @@ export default class Application extends Component {
                   key="teal"
                   component={TealScene}
                   title="Teal Scene"
+                />
+              </Scene>
+
+              {/* Tab and it's scenes */}
+              <Scene key="baseTab" title="Base" icon={TabIcon}>
+                <Scene
+                  key="base"
+                  component={Base}
+                  title="Base Component"
                 />
               </Scene>
             </Scene>
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,11 +1,7 @@
 import { combineReducers } from 'redux';
 
 import base from './base';
-import counter from './counter';
-import todo from './todo';
 
 export default combineReducers({
   base,
-  counter,
-  todo,
 });
