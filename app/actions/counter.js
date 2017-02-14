6772894d47732a7f964ebb522db3c00b5ac1f94e
easydaniel <whitetiger50512@gmail.com>
2017-02-15 00:19:42 +0800

    basic template

--- app/actions/counter.js
+++
@@ -1,6 +0,0 @@
-import { createAction } from 'redux-actions';
-
-
-export const increase = createAction('INCREASE');
-export const decrease = createAction('DECREASE');
-export const reset = createAction('RESET');
